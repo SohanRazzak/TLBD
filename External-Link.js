2fbe744f90f6ef7086d05332eeaabbeea1870e64
Sohan Razzak <sohanrazzak7@gmail.com>
2021-12-22 19:34:40 +0600

    Create External-Link.js

---
+++ External-Link.js
@@ -0,0 +1,33 @@
+const modalButton = document.querySelector(".ex-link");
+const extra = document.createElement("div");
+const modalhtml = `<div class="modal-header">
+&#128279; আপনি আমাদের সাইট ছেড়ে যাচ্ছেন!
+<span class="close-modal" onclick="closeModal()">&times</span>
+</div>
+<div class="modal-body">
+এটি একটি এক্সটারনাল সাইটের লিংক
+<br/><br/><div class="show-link"></div><br/>
+লিংকে যেতে নিচের যান বাটনে ক্লিক করুন। আপনি যদি পুরো পোষ্ট না পড়ে থাকে,তাহলে থাকুন বাটনে ক্লিক করে ফিরে আসতে পারবেন। আমাদের অনান্য পোষ্ট পড়তে ভুলবেন না। ধন্যবাদ 😀
+</div>
+<div class="modal-footer">
+<button class="close-modal" onclick="closeModal()">থাকুন </button>
+<button class="confirm-modal">যান</>
+</div>`;
+var openModal;
+if (modalButton)
+  { 
+  openModal = function () {
+  let getlink = event.currentTarget.dataset.href;
+  extra.classList.add("modal-content");
+  extra.innerHTML = modalhtml;
+  document.body.appendChild(extra);
+  document.querySelector(".show-link").innerHTML = "(" + getlink + ")";  
+  document.querySelector(".confirm-modal").addEventListener("click", (e) => {
+    window.open(getlink, "_blank");
+    extra.remove();
+  });  
+  };
+}
+function closeModal() {
+  extra.remove();
+}
