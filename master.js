cab7e66f66a69bbdde6985eeed248d356c10d3a4
Sohan Razzak <sohanrazzak7@gmail.com>
2025-08-18 12:07:52 +0600

    Update master.js

--- master.js
+++ master.js
@@ -14,9 +14,9 @@ if (_d.includes(_h) === false) {
     const o = document.createElement("div");
     o.style.cssText = `
         position:fixed;top:0;left:0;width:100%;height:100%;
-        background:#000;color:#fff;font-size:20px;
+        background:#000;color:#fff;font-size:24px;
         display:flex;align-items:center;justify-content:center;
-        z-index:999999;overflow-y: hidden;
+        z-index:999999;overflow-y:hidden!important;
     `;
     o.textContent = "⚠ Unauthorized domain";
     document.body.appendChild(o);
